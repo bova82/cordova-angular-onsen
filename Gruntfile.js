6d134972cd4fef6172d337477be08b426cab5f2d
Giovanni Pini <giovanni.pini@phoops.it>
2015-12-22 13:02:34 +0100

    Introduce folder variables

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,36 @@
 module.exports = function(grunt) {
 
+  // Set folders reused throughout the script
+  var SRC_FOLDER = 'app';
+  var DIST_FOLDER = 'www';
+  var SRC_SASS_REL = 'sass';
+  var SRC_CSS_REL = 'css';
+  var SRC_JS_REL = 'js';
+  var SRC_IMG_REL = 'img';
+  var SRC_FONTS_REL = 'fonts';
+  var DIST_CSS_REL = 'styles';
+  var DIST_JS_REL = 'scripts';
+  var DIST_IMG_REL = 'img';
+  var DIST_FONTS_REL = 'fonts';
+  var paths = {
+    src: SRC_FOLDER,
+    dist: DIST_FOLDER,
+    src_css: SRC_FOLDER + SRC_CSS_REL,
+    src_sass: SRC_FOLDER + SRC_SASS_REL,
+    src_js: SRC_FOLDER + SRC_JS_REL,
+    src_img: SRC_FOLDER + SRC_IMG_REL,
+    src_fonts: SRC_FOLDER + SRC_FONTS_REL,
+    src_img_rel: SRC_IMG_REL,
+    src_fonts_rel: SRC_FONTS_REL,
+    dist_css: DIST_FOLDER + DIST_CSS_REL,
+    dist_js: DIST_FOLDER + DIST_JS_REL,
+    dist_img: DIST_FOLDER + DIST_IMG_REL,
+    dist_fonts: DIST_FOLDER + DIST_FONTS_REL
+  };
+
   // Project configuration.
   grunt.initConfig({
+    paths: paths,
     pkg: grunt.file.readJSON('package.json'),
 
     clean: {
@@ -10,34 +39,33 @@ module.exports = function(grunt) {
           dot: true,
           src: [
             '.tmp',
-            'app/css/*.css',
-            'www/{,*/}*',
-            'www/.git{,*/}*'
+            '<%= paths.dist %>/{,*/}*',
+            '<%= paths.dist %>/.git{,*/}*'
           ]
         }]
       },
       css: {
         files: [{
-          src: ['app/css/*.css']
+          src: ['<%= paths.src_css %>/*.css']
         }]
       }
     },
 
     watch: {
       js: {
-        files: ['app/**/*.js'],
+        files: ['<%= paths.src_js %>/*.js'],
         tasks: [],
         options: {
           livereload: true
         }
       },
       html : {
-        files: ['app/**/*.html'],
+        files: ['<%= paths.src %>/**/*.html'],
         tasks: ['wiredep'],
         options: {
           livereload: true
         }
-      }
+      },
       bower: {
         files: ['bower.json'],
         tasks: ['wiredep'],
@@ -46,7 +74,7 @@ module.exports = function(grunt) {
         }
       },
       sass: {
-        files: ['app/sass/*.sass', 'app/sass/*.scss'],
+        files: ['<%= paths.src_sass %>/*.sass', 'app/sass/*.scss'],
         tasks: ['clean:css', 'compass'],
         options: {
           livereload: true
@@ -57,9 +85,9 @@ module.exports = function(grunt) {
     copy: {
       all: {
         expand: true,
-        cwd: 'app/',
-        src: ['**/*.html', 'img/**/*', 'fonts/**/*'],
-        dest: 'www/'
+        cwd: '<%= paths.src %>',
+        src: ['**/*.html', '<%= paths.src_img_rel %>/**/*', '<%= paths.src_fonts_rel %>/**/*'],
+        dest: '<%= paths.dist %>'
       }
     },
 
@@ -67,10 +95,10 @@ module.exports = function(grunt) {
     filerev: {
       dist: {
         src: [
-          'www/scripts/{,*/}*.js',
-          'www/styles/{,*/}*.css',
-          'www/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          'www/fonts/*',
+          '<%= paths.dist_js %>/{,*/}*.js',
+          '<%= paths.dist_css %>/{,*/}*.css',
+          '<%= paths.dist_img %>/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= paths.dist_fonts %>/*',
         ]
       }
     },
@@ -78,16 +106,16 @@ module.exports = function(grunt) {
     compass : {
       dist: {
         options: {
-          sassDir: 'app/sass',
-          cssDir: 'app/css'
+          sassDir: '<%= paths.src_sass %>',
+          cssDir: '<%= paths.src_css %>'
         }
       }
     },
 
     useminPrepare: {
-      html: 'app/index.html',
+      html: '<%= paths.src %>/index.html',
       options: {
-        dest: 'www/',
+        dest: '<%= paths.dist %>',
         flow: {
           html: {
             steps: {
@@ -102,14 +130,14 @@ module.exports = function(grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['www/{,*/}*.html'],
-      css: ['www/styles/{,*/}*.css'],
-      js: ['www/scripts/{,*/}*.js'],
+      html: ['<%= paths.dist %>/{,*/}*.html'],
+      css: ['<%= paths.dist_css %>/{,*/}*.css'],
+      js: ['<%= paths.dist_js %>/{,*/}*.js'],
       options: {
         assetsDirs: [
-          'www/',
-          'www/img',
-          'www/css'
+          '<%= paths.dist %>',
+          '<%= paths.dist_img %>',
+          '<%= paths.dist_css %>'
         ],
         patterns: {
           html: [[/(img\/[^''""]*\.(png|jpg|jpeg|gif|webp|svg))/g, 'Replacing references to images']]
@@ -119,7 +147,7 @@ module.exports = function(grunt) {
 
     wiredep : {
       task: {
-        src: 'app/index.html',
+        src: '<%= paths.src %>/index.html',
         options: {
             overrides: {
               "OnsenUI": {
